5514d36f9630db6d502e6dee5422257513c966f5
Stian Weie <stianwe@gmail.com>
2015-02-23 23:39:11 +0100

    Initial projectile scripts

---
+++ Assets/ProjectileSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileSpawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public BaseProjectile SpawnProjectile(Object projectilePrefab, Vector3 spawnPosition, Vector2 direction)
+    {
+        Instantiate(projectilePrefab, spawnPosition, Quaternion.Euler())
+    }
+}
