5caf8533ae6e33cd10c10a8e10c7afeb5223260f
Stian Weie <stianwe@gmail.com>
2015-02-28 14:23:27 +0100

    Working projectile based on direction

---
+++ Assets/Scripts/BaseProjectile.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseProjectile : MonoBehaviour
+{
+
+    public float InitialForce = 1000;
+
+    private bool _gDown = false;
+
+	// Use this for initialization
+	void Start () {
+        //Debug.Log("Adding " + InitialForce + " force to projectile.");
+        //this.rigidbody.AddForce(this.transform.forward * InitialForce);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("g"))
+        {
+            _gDown = true;
+        }
+        else if (_gDown)
+        {
+            _gDown = false;
+            Debug.Log("Adding force to projectile");
+            this.rigidbody.AddForce(this.transform.forward * InitialForce);
+        }
+	}
+}
---
+++ Assets/Scripts/ProjectileSpawner.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileSpawner : MonoBehaviour
+{
+
+    public Transform TestProjectile;
+    public Transform ExitPoint;
+    public float SpawnDistance = 1f;
+    public Vector2 InitialDirection = new Vector2(0, 1);
+
+    public Vector2 TestSpawnDirection = new Vector2(1, 0);
+
+    private bool _fDown = false;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if (Input.GetKey("f"))
+	    {
+	        _fDown = true;
+	    }
+	    else if (_fDown)
+        {
+            _fDown = false;
+            SpawnProjectile(TestProjectile, ExitPoint.position, TestSpawnDirection);
+        }
+	}
+
+    private float DirectionToRotation(Vector2 direction)
+    {
+        var angle = Vector2.Angle(direction, InitialDirection);
+        if (direction.x < 0)
+        {
+            angle = 360 - angle;
+        }
+        Debug.Log("The angle between " + InitialDirection + " and " + direction + " is " + angle);
+        return angle;
+    }
+
+    public BaseProjectile SpawnProjectile(Object projectilePrefab, Vector3 spawnPosition, Vector2 direction)
+    {
+        //direction.
+        var rotation = Quaternion.Euler(0, DirectionToRotation(direction), 0);
+        var position = spawnPosition + new Vector3(direction.x * SpawnDistance, 0, direction.y * SpawnDistance);
+        Debug.Log("Spawning projectile at " + position + ", with rotation: " + rotation.eulerAngles);
+        return (Instantiate(projectilePrefab, position, rotation) as Transform).gameObject.GetComponent<BaseProjectile>();
+    }
+}
