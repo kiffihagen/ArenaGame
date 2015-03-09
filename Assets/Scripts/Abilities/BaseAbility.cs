d51f6e88bf2bbf7d00c71b7dc19e6b5f559fd540
Stian Weie <stianwe@gmail.com>
2015-03-09 20:43:32 +0100

    Basic ability cycling functionality

---
+++ Assets/Scripts/Abilities/BaseAbility.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseAbility : MonoBehaviour
+{
+
+    public string AbilityName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Use()
+    {
+        Debug.Log("Using ability " + AbilityName);
+    }
+}
---
+++ Assets/Scripts/AbilityStack.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AbilityStack : MonoBehaviour {
+
+    private List<BaseAbility> _abilities = new List<BaseAbility>();
+
+    private int _abilityCycleIndex = 0;
+
+    public float TimeBetweenAbilityCycling = 1f;
+
+    private float _timeOfLastAbilityCycling = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Time.time >= _timeOfLastAbilityCycling + TimeBetweenAbilityCycling)
+	    {
+	        CycleAbilities();
+	    }
+	}
+
+    private void CycleAbilities()
+    {
+        _timeOfLastAbilityCycling = Time.time;
+        _abilityCycleIndex = (_abilityCycleIndex + 1) % _abilities.Count;
+    }
+
+    public void UseAbility()
+    {
+        _abilities[_abilityCycleIndex].Use();
+    }
+
+    public void AddAbility(BaseAbility ability)
+    {
+        _abilities.Add(ability);
+    }
+}
